9c0772846565a7b51f95dd7b83d16ef81aeee6da
SNJack <114724153+SNJack0911@users.noreply.github.com>
2022-10-18 09:46:31 +0200

    Update Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,9 +4,9 @@ public class Romain {
 	private String nom;
 	private int force;
 	public Romain(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+		assert force>0;
 	}
 	
 	public String getNom() {
@@ -17,11 +17,13 @@ public class Romain {
 	}
 
 	private String prendreParole() {
-		// TODO Auto-generated method stub
 		return "Le romain "+nom +":";
 	}
 	public void recevoirCoup(int forceCoup) {
+		assert force>0;
+		int initForce=force;
 		force -=forceCoup;
+		assert initForce>force;
 		if (force>0) {
 			parler("Aie");
 		} else {
